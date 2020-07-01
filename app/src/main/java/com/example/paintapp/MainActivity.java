4c122b4aa3b8f6289a96b88a329bbe387fc2ac4b
Faisal <32745275+fmshk97@users.noreply.github.com>
2020-07-01 19:29:13 +0530

    Changes by Faisal (#2)
    * Deleted un-necessary files and updated gitignore
    
    Deleted un-necessary files and updated gitignore
    
    * Bug Fix :)
    
    * Close drawer on menu item select

--- app/src/main/java/com/example/paintapp/MainActivity.java
+++ app/src/main/java/com/example/paintapp/MainActivity.java
@@ -13,7 +13,10 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.material.navigation.NavigationView;
 
@@ -28,6 +31,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     View contentView;
     ImageView pencil,eraser,undo,redo;
     private ActionBarDrawerToggle actionBarDrawerToggle;
+    private FragmentManager fragmentManager;
+    private FragmentTransaction fragmentTransaction;
 
 
     @Override
@@ -72,6 +77,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void UserMenuSelected(MenuItem menuItem)
     {
+        drawer.closeDrawer(GravityCompat.START);
         switch (menuItem.getItemId())
         {
             case R.id.Pencil:
@@ -79,7 +85,10 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         "pencil selected",
                         Toast.LENGTH_SHORT);
                 toast.show();
-               setContentView(canvasObj);
+                fragmentManager = getSupportFragmentManager();
+                fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.holder, new PencilFragment());
+                fragmentTransaction.addToBackStack(null).commit();
                 break;
             case R.id.Eraser:
                 Toast toast1 = Toast.makeText(getApplicationContext(),
